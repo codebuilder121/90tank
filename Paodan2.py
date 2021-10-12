8b78af9d51a7cd2cc3ff09a425fc8db34a5f0f5a
codebuilder121 <zht_8@126.com>
2021-10-12 17:31:35 +0800

    Revert "Paodan2.py"
    This reverts commit 9af0c9eb

---
+++ Paodan2.py
@@ -0,0 +1,12 @@
+import pygame
+
+
+class Pao2(object):
+    def __init__(self,paopos,Direction,source,speed,breakdown_ice):
+        self.paopos = paopos
+        self.Direction =Direction
+        self.source=source
+        self.speed=speed
+        self.breakdown_ice=breakdown_ice
+
+
