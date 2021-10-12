5e042fe15d9ebaa38fb733e37aa387966b910c18
codebuilder121 <zht_8@126.com>
2021-10-12 17:56:17 +0800

    Revert "Revert "first commit""
    This reverts commit c3fcce9e

---
+++ Paodan3.py
@@ -0,0 +1,12 @@
+import pygame
+
+
+class Pao(object):
+    def __init__(self,paopos,Direction,source,speed,breakdown_ice):
+        self.paopos = paopos
+        self.Direction =Direction
+        self.source=source
+        self.speed=speed
+        self.breakdown_ice=breakdown_ice
+
+
